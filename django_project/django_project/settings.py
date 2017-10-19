3d1cc4b55a08d49384dce42bf8c3c1c8595aa2a3
patrik wagner <patrik.wagner@epfl.ch>
2017-10-19 15:39:41 +0200

    global changes in main settings, urls, and gitignore

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'interface_without_scenedetect'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# media path
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from test_blog import views
+from interface_without_scenedetect import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^test_blog/', include('test_blog.urls'))
+    url(r'^test_blog/', include('test_blog.urls')),
+    url(r'^interface/', include('interface_without_scenedetect.urls')),
 ]
 
 
