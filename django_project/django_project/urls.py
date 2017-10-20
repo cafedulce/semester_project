bd036df72acb0646333bf80c8a724ec052d40637
patrik wagner <patrik.wagner@epfl.ch>
2017-10-20 13:09:17 +0200

    changes in dbsql and urls.py

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from test_blog import views
+
 from interface import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^test_blog/', include('test_blog.urls')),
     url(r'^interface/', include('interface.urls')),
 ]
 
