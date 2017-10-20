a8a9307a1f28bfbf9ed1b78abd6d82e8a05fca10
patrik wagner <patrik.wagner@epfl.ch>
2017-10-20 12:43:32 +0200

    working import scenedetect

--- django_project/interface/models.py
+++ django_project/interface/models.py
@@ -8,3 +8,50 @@ class VideoFile(models.Model):
 
     def __str__ (self):
         return self.name
+
+"""class PySceneDetectArgs(object):
+    def __init__(self, input, type='content', threshold=None, save_im=False, mode=True, stats=None):
+        self.input = input
+        self.detection_method = type
+        self.threshold = threshold
+        self.min_percent = 95
+        self.min_scene_len = 15
+        self.block_size = 8
+        self.fade_bias = 0
+        self.downscale_factor = 1
+        self.frame_skip = 2
+        self.save_images = save_im
+        self.start_time = None
+        self.end_time = None
+        self.duration = None
+        self.quiet_mode = mode
+        self.stats_file = stats
+
+scene_detectors = scenedetect.detectors.get_available()
+
+smgr_content = scenedetect.manager.SceneManager(
+    PySceneDetectArgs(input=path, type='content', threshold=30, save_im=False, mode=True), scene_detectors)
+
+fps, read, processed = scenedetect.detect_scenes_file(path=path, scene_manager=smgr_content)"""
+
+class PySceneDetectArgs(models.Model):
+
+    name = models.CharField(max_length=255)
+    detection_method = models.CharField(max_length=255, default='content')
+    threshold = models.IntegerField(default=30)
+    min_percent = models.IntegerField(default=95)
+    min_scene_len = models.IntegerField(default=15)
+    block_size = models.IntegerField(default=8)
+    fade_bias = models.IntegerField(default=0)
+    downscale_factor = models.IntegerField(default=1)
+    frame_skip = models.IntegerField(default=2)
+    save_images = models.BooleanField(default=False)
+    quiet_mode = models.BooleanField(default=True)
+    stats_file = models.BooleanField(default=False)
+    start_time = models.IntegerField(null=True)
+    end_time = models.IntegerField(null=True)
+    duration = models.IntegerField(null=True)
+
+
+    def __str__(self):
+        return self.name
--- django_project/interface/views.py
+++ django_project/interface/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 
 from interface.forms import VideoForm
 from interface.models import VideoFile
+from interface.models import PySceneDetectArgs
+
+import scenedetect
 
 # Create your views here.
 
@@ -20,11 +23,30 @@ def upload(request):
     uploaded=False
     form = VideoForm(request.POST or None, request.FILES)
 
+    fps, read, processed = 0,0,0
+
     if form.is_valid():
         video_file = VideoFile()
         video_file.video = form.cleaned_data["video"]
-        video_file.name = form.cleaned_data["name"]
         video_file.save()
         uploaded=True
 
-    return render(request, 'interface/upload.html', {'form': form, 'uploaded': uploaded})
+        scenedetect_object = PySceneDetectArgs()
+        scenedetect_object.name = (video_file.video.path)
+        scenedetect_object.type = 'content'
+        scenedetect_object.threshold = 30
+        scenedetect_object.save_images = False
+        scenedetect_object.quiet_mode = True
+        scenedetect_object.stats_file = False
+        scenedetect_object.start_time = None
+        scenedetect_object.end_time = None
+        scenedetect_object.duration = None
+
+        scene_detectors = scenedetect.detectors.get_available()
+
+        smgr_content = scenedetect.manager.SceneManager(scenedetect_object, scene_detectors)
+
+        fps, read, processed = scenedetect.detect_scenes_file(path=scenedetect_object.name, scene_manager=smgr_content)
+
+
+    return render(request, 'interface/upload.html', {'form': form, 'uploaded': uploaded, 'fps':fps,'read':read, 'processed':processed})
